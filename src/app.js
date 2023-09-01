296ca5fc2a6b9b44a73ee1260233b44b23e3e345
Aparnaa-k <aparnaa.kumar@kalvium.community>
2023-09-01 10:12:29 +0530

    added code in js file

--- src/app.js
+++ src/app.js
@@ -25,6 +25,11 @@ const PLAYERS = [
 class Player {
   constructor(id, name, type) {
     // Progression 1: Create member variables and assign values
+    this.id = id;
+    this.name = name;
+    this.strength = this.getRandomStrength()
+    this.image = `/images/super-${id}.png`
+    this.type = type; 
   }
   // getting random strength
   getRandomStrength = () => {
@@ -35,6 +40,26 @@ class Player {
   view = () => {
     // Accumulate HTML template
     // Type your code here
+    let id = this.id + 1
+    const player = document.createElement('div');
+    player.classList.add('player');
+    player.setAttribute('data-id', id);
+    
+    const playerImage = document.createElement('img');
+    playerImage.setAttribute('src', `images/super-${id}.png`);
+    
+    const playerName = document.createElement('div');
+    playerName.classList.add('name');
+    playerName.textContent = this.name;
+    
+    const playerStrength = document.createElement('div');
+    playerStrength.classList.add('strength');
+    playerStrength.textContent = this.strength;
+    
+    player.appendChild(playerImage);
+    player.appendChild(playerName);
+    player.appendChild(playerStrength);
+    
 
     return player;
   };
@@ -47,6 +72,10 @@ class Superwar {
     // Create a field players
     // Use Map method to loop through players argument and create new players
     // Type your code here
+    this.players = players.map((item,index)=>{
+      let type = (index%2==0)?"hero":"villain"
+      return new Player(index,item,type)
+    })
   }
 
   // Display players in HTML
@@ -73,7 +102,7 @@ class Superwar {
 }
 
 // uncomment this part -- only after you complete progression 3
-// window.onload = () => {
-//     const superwar = new Superwar(PLAYERS);
-//     superwar.viewPlayers();
-// }
+window.onload = () => {
+    const superwar = new Superwar(PLAYERS);
+    superwar.viewPlayers();
+}
